95b2ca77039102479bed9228d707a290f2e4051e
AbrahamJ23 <74594619+AbrahamJ23@users.noreply.github.com>
2023-04-06 12:41:34 +0200

    basic pages

---
+++ app.py
@@ -0,0 +1,41 @@
+
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+import esptool
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mijngeheimesleutel'
+
+class InfoForm(FlaskForm):
+    instrument = StringField("Welk instrument wil je graag leren bespelen?")
+    Submit = SubmitField("verzend")
+naam = "Abraham"
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    # render de template home.html
+    form = InfoForm()
+    # haal de data voor instrumenten uit het formulier
+    if form.validate_on_submit():
+        instrument = form.instrument.data
+        form.instrument.data = ''
+        return render_template("index.html", form = form, instrument = instrument)
+
+@app.route("/informatie")
+def info():
+    #render de template informatie
+    return "<h1>Dit hebben we jou te bieden</h1>"
+
+@app.route("/random_pagina/<naam>")
+def random_pagina(naam):
+    return f"gebruiker: {naam}" + f"<h1> Dit is de pagina van {naam}"
+
+@app.route("/cursist/<naam>")
+def cursist(naam):
+    test = ["voetbal", "Dansen", "tennis"]
+    return render_template("index.html", naam=naam, test=test)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+# form gedeelte werkt nog niet!
