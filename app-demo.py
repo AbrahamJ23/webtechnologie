0270b558c5ac7c29adad95bee8da2cd86c34c129
AbrahamJ23 <74594619+AbrahamJ23@users.noreply.github.com>
2023-04-10 18:27:39 +0200

    changes

--- app-demo.py
+++ app-demo.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -8,10 +9,11 @@ app.config['SECRET_KEY'] = 'mijngeheimesleutel'
 class InfoForm(FlaskForm):
     instrument = StringField("Welk instrument wil je graag leren bespelen?")
     Submit = SubmitField("verzend")
-    naam = "Abraham"
+naam = "Abraham"
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    instrument = False
     # render de template home.html
     form = InfoForm()
     # haal de data voor instrumenten uit het formulier
@@ -19,3 +21,22 @@ def index():
         instrument = form.instrument.data
         form.instrument.data = ''
     return render_template("index.html", form=form, instrument=instrument) 
+
+@app.route("/informatie")
+def info():
+    #render de template informatie
+    return "<h1>Dit hebben we jou te bieden</h1>"
+
+@app.route("/random_pagina/<naam>")
+def random_pagina(naam):
+    return f"gebruiker: {naam}" + f"<h1> Dit is de pagina van {naam}"
+
+@app.route("/cursist/<naam>")
+def cursist(naam):
+    form = InfoForm()
+    test = ["voetbal", "Dansen", "tennis"]
+    return render_template("index.html", naam=naam, test=test)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+# form gedeelte werkt nog niet!
