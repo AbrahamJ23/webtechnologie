a396edad2b48e9e6a30c773be0a79d4f0515a2e4
AbrahamJ23 <74594619+AbrahamJ23@users.noreply.github.com>
2023-04-13 02:02:14 +0200

    redirect werkt niet + nog db adden

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, DateField, SelectField
 
 
 app = Flask(__name__)
@@ -8,29 +8,43 @@ app.config['SECRET_KEY'] = 'mijngeheimesleutel'
 
 
 class InfoForm(FlaskForm):
-    check_in = StringField()
-    instrument = StringField("Welk instrument wil je graag leren bespelen?")
-    Submit = SubmitField("verzend")
-    naam = "Abraham"
+    check_in = DateField()
+    check_out = DateField()
+    huisje = SelectField(u'Welk huisje?', choices=[('Huis 1','4 personen'), ('Huis 2', '6 Personen'), ('Huis 3', '8 Personen')])
+    submit = SubmitField('Verzend')
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    instrument = False
-    # render de template home.html
     form = InfoForm()
-    # haal de data voor instrumenten uit het formulier
     if form.validate_on_submit():
-        instrument = form.instrument.data
-        form.instrument.data = ''
-    return render_template("index.html", form=form, instrument=instrument) 
+        session['check_in'] = form.check_in.data
+        session['check_out'] = form.check_out.data
+        session['huisje'] = form.huisje.data
 
-@app.route("/test", methods=['GET', 'POST'])
-def test():
-    check_in=False
-    if request.method == "POST":
-        check_in = request.form.get("checkin")
-    return render_template("hotel-html-template/index.html", check_in=check_in)
+        return redirect(url_for('bedankt'))
+    
+    return render_template("home.html", form=form) 
+
+@app.route("/bedankt")
+def bedankt():
+    return render_template("bedankt.html")
+
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+# @app.route("/", methods=['GET', 'POST'])
+# def index():
+#     instrument = False
+#     # render de template home.html
+#     form = InfoForm()
+#     # haal de data voor instrumenten uit het formulier
+#     if form.validate_on_submit():
+#         instrument = form.instrument.data
+#         form.instrument.data = ''
+#     return render_template("home.html", form=form, instrument=instrument) 
